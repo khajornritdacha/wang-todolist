c61936fda99ae605b00c394d32d3b0ae3ba903be
Khajornritdadcha <vijak13019@hotmail.com>
2023-06-03 09:33:13 +0700

    feat(navbar): add logout icon

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,14 +1,12 @@
-import Hamburger from "./Hamburger/Hamburger";
+import { FiLogOut } from "react-icons/fi";
 import styles from "./Navbar.module.css";
-import { SlMagnifier } from "react-icons/sl";
 import { NAVBAR_HEIGHT } from "./constants";
 
 export default function Navbar() {
   return (
     <nav className={styles.navbarContainer} style={{ height: NAVBAR_HEIGHT }}>
-      <Hamburger />
       <h1>TodoList</h1>
-      <SlMagnifier />
+      <FiLogOut className={styles.icon} />
     </nav>
   );
 }
